da66f24db4ca3fe2fd4637f65e36e569ff1de8bb
César Fernández <kaesar84@icloud.com>
2023-09-05 23:06:18 +0200

    First

---
+++ index.php
@@ -0,0 +1,53 @@
+</html>
+
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+    <link rel="stylesheet" href="styles/css/styles.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Fira+Code:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <nav class="navbar bg-dark border-bottom border-body" data-bs-theme="dark">
+        <!-- Navbar content -->
+    </nav>
+
+
+
+
+    <!-- TERMINAL   -->
+    <div class="container">
+        <div class="container codeWindow">
+            <div class="codeNav">🔴🟡🟢 </div>
+            <div class="codeContent">
+                <h5>Titulo</h5>
+                <p> Lorem ipsum dolor sit amet consectetur adipiscing elit inceptos curae venenatis, tincidunt tempus facilisi <span>mus pretium sagittis</span>  eleifend ut leo neque, nec litora accumsan urna vitae torquent ornare donec sem. Habitant dictumst orci dapibus luctus dis condimentum velit ut felis enim eget nulla ullamcorper, primis faucibus vehicula pulvinar varius libero dui laoreet ornare sed porttitor rhoncus, elementum nisi curae augue suspendisse mattis integer nostra netus duis cursus in. Ullamcorper hac pharetra aliquam lectus egestas neque class, volutpat id cras inceptos porta parturient per, duis in lacinia etiam rutrum congue.</p>
+            </div>
+        </div>
+    </div>
+
+    <!-- NAVEGADOR   -->
+    <div class="container">
+        <div class="container resutlWindow">
+            <div class="resultNav">🔴🟡🟢 </div>
+            <div class="resultContent">
+                <h5>Titulo</h5>
+                <p> Lorem ipsum dolor sit amet consectetur adipiscing elit inceptos curae venenatis, tincidunt tempus facilisi mus pretium sagittis eleifend ut leo neque, nec litora accumsan urna vitae <span> torquent ornare donec sem</span>. Habitant dictumst orci dapibus luctus dis condimentum velit ut felis enim eget nulla ullamcorper, primis faucibus vehicula pulvinar varius libero dui laoreet ornare sed porttitor rhoncus, elementum nisi curae augue suspendisse mattis integer nostra netus duis cursus in. Ullamcorper hac pharetra aliquam lectus egestas neque class, volutpat id cras inceptos porta parturient per, duis in lacinia etiam rutrum congue.</p>
+            </div>
+        </div>
+    </div>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+</body>
+
+</html>
